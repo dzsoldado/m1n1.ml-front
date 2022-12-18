be82647f39127e4f9df9c927fb2d4fb80d28d1c8
dzsoldado <klemidz@gmail.com>
2022-12-18 22:25:53 +0100

    first tracked commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Profile from './pages/Profile';
+import Landing from './pages/Landing';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+
+          <Route path='/' element={
+            <Landing/>
+          }/>
+          <Route path='/signin' element={
+            <SignIn/>
+          }/>
+          <Route path='/signup' element={
+            <SignUp/>
+          }/>
+          <Route path='/profile' element={
+            <Profile/>
+          }/>
+          <Route path='*' element={
+            <h1>Page not found 404</h1>
+          }/>
+
+        </Routes>
+      </Router>
+    </>
+
   );
 }
 
