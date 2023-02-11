2ac1a80c5f8ee5aaf1adfa1ec5580daf4f0d12d5
oualid.b <klemidz@gmail.com>
2023-02-11 22:16:10 +0100

    fixed responsive table issue

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Link, Button, CircularProgress, Backdrop } from '@mui/material/';
+import { Box, Table, TableBody, TableCell, Container, TableHead, TableRow, Link, Button, CircularProgress, Backdrop } from '@mui/material/';
 
 import { getLinks, deleteLink } from '../FirebaseDB';
 
@@ -40,51 +40,50 @@ export default function LinksTable() {
 
   return (
     <>
-      <TableContainer component={Paper} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Table sx={{ maxWidth: "md" }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ minWidth: 100, maxWidth: 150 }}>Original Link</TableCell>
-              <TableCell style={{ minWidth: 100, maxWidth: 150 }}>Short Link</TableCell>
-              <TableCell style={{ minWidth: 100, maxWidth: 150 }}>Creation Date</TableCell>
-              <TableCell align="center" style={{ minWidth: 100, maxWidth: 150 }}>Total Clicks</TableCell>
-              <TableCell style={{ minWidth: 100, maxWidth: 150 }}></TableCell>
-              <TableCell style={{ minWidth: 100, maxWidth: 150 }}></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.id}
-              >
+      <Container maxWidth="md">
+        <Box sx={{ overflow: "auto", margin: '5rem 0' }}>
+          <Box sx={{ width: '100%', display: 'table', tableLayout: 'fixed', boxShadow: 'none' }}>
+            <Table sx={{ maxWidth: "md" }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ minWidth: 100, maxWidth: 150 }}>Original Link</TableCell>
+                  <TableCell style={{ minWidth: 100, maxWidth: 150 }}>Short Link</TableCell>
+                  <TableCell style={{ minWidth: 100, maxWidth: 150 }}>Creation Date</TableCell>
+                  <TableCell align="center" style={{ minWidth: 100, maxWidth: 150 }}>Total Clicks</TableCell>
+                  <TableCell style={{ minWidth: 80, maxWidth: 120 }}></TableCell>
+                  <TableCell style={{ minWidth: 80, maxWidth: 120 }}></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                  >
 
-                <TableCell style={{ minWidth: 100, maxWidth: 150 }}><Link href={row.short_link} target='_blanc' noopener='true'>{row.short_link}</Link></TableCell>
-                <TableCell title={row.original_link} style={{ minWidth: 100, maxWidth: 150, ...css_TruncateText }} >
-                  {row.original_link}
-                </TableCell>
-                <TableCell style={{ minWidth: 100, maxWidth: 150 }}>{row.created_at}</TableCell>
-                <TableCell align="center" style={{ minWidth: 100, maxWidth: 150 }}>{row.clicks_count}</TableCell>
-                <TableCell align="center" style={{ minWidth: 100, maxWidth: 150 }}>
-                  <Button >
-                    Details
-                  </Button>
-                </TableCell>
-                <TableCell align="center" style={{ minWidth: 100, maxWidth: 150 }}>
-                  <Button onClick={() => handleDelete(row.id)}>
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={isLoading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
+                    <TableCell style={{ minWidth: 100, maxWidth: 150 }}><Link href={row.short_link} target='_blanc' noopener='true'>{row.short_link}</Link></TableCell>
+                    <TableCell title={row.original_link} style={{ minWidth: 100, maxWidth: 150, ...css_TruncateText }} >
+                      {row.original_link}
+                    </TableCell>
+                    <TableCell style={{ minWidth: 100, maxWidth: 150 }}>{row.created_at}</TableCell>
+                    <TableCell align="center" style={{ minWidth: 100, maxWidth: 150 }}>{row.clicks_count}</TableCell>
+                    <TableCell align="center" style={{ minWidth: 80, maxWidth: 120 }}>
+                      <Button >
+                        Details
+                      </Button>
+                    </TableCell>
+                    <TableCell align="center" style={{ minWidth: 80, maxWidth: 120 }}>
+                      <Button onClick={() => handleDelete(row.id)}>
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </Box>
+        {isLoading && <CircularProgress color="inherit" />}
+      </Container>
     </>
   );
 }
